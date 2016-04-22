8659540a55b14d011d5f82bbd345d129b34e49ef
Heath Reineke <heathreineke2018@u.northwestern.edu>
2016-04-21 21:47:26 -0500

    Updated findCup

--- tests/findCup/findCup.ino
+++ tests/findCup/findCup.ino
@@ -5,6 +5,7 @@
 #define LEFTSPD 8
 #define LEFTDIR 9
 #define TRANS A0
+#define LASER 2
 
 
 //initialize pins
@@ -15,6 +16,23 @@ void setup() {
   pinMode(LEFTSPD, OUTPUT);
   pinMode(LEFTDIR, OUTPUT);
   digitalWrite(LEFTDIR, LOW);
+  pinMode(LASER, OUTPUT);
+  digitalWrite(LASER, HIGH);
+}
+
+//basic go to cup function
+void attackCup() {
+   int base = analogRead(TRANS);//base reading
+   int cont = analogRead(TRANS);//reading that will be updated
+
+  //loop continues while cup is still detected
+   while (cont >= (base - 50)) {
+    digitalWrite(LEFTDIR, LOW);
+    analogWrite(LEFTSPD, 100);
+    analogWrite(RIGHTSPD, 100);
+    delay(500);//may be able to play with delay length
+    cont = analogRead(TRANS);
+  }
 }
 
 void loop() {
@@ -23,19 +41,13 @@ int original = analogRead(TRANS);//read transistor for baseline
 digitalWrite(LEFTDIR, HIGH);
 analogWrite(LEFTSPD, 50);
 analogWrite(RIGHTSPD, 50);
-delay(500);//turn counterclockwise for .5 sec
-analogWrite(LEFTSPD, LOW);
-analogWrite(RIGHTSPD, LOW);
-delay(500);
+//turn counterclockwise
 int next = analogRead(TRANS);//read transistor again
 
 //check to see if new reading is significantly higher
 //if so, move toward cup
 if (next > (original + 50)) {
-  digitalWrite(LEFTDIR, LOW);
-  analogWrite(LEFTSPD, 100);
-  analogWrite(RIGHTSPD, 100);
-  delay(500);
-}
+  attackCup();
+  }
 
 }
